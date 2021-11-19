13ab0d36d42b5f5c606c2a34353aff1959bffe8e
vamsi <vamsimothall@gmail.com>
2021-11-19 14:21:37 +0530

    initial

--- app.py
+++ app.py
@@ -9,67 +9,65 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
     if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        seconds, error = get_total_seconds(f.filename)
-        os.remove(f.filename)
-        if not error == '':
-            error_title = "The following errors are found in the file"
+        file = request.files['file']
+        file.save(secure_filename(file.filename))
+        seconds, log = get_seconds_from_log_file(file.filename)
+        if not log == '':
+            shown = True
         else:
-            error_title = ""
-        return render_template("output.html", output=convert_to_hours(seconds), error=error, error_title=error_title)
+            shown = False
+        os.remove(file.filename)
+        return render_template("console.html", output=convert(seconds), log=log, shown=shown)
 
 
-def convert_to_hours(secs):
-    mints, sec = divmod(secs, 60)
-    hour, mints = divmod(mints, 60)
-    return "%d hours %d minutes" % (hour, mints)
-
-
-def get_total_seconds(file_path):
+def get_seconds_from_log_file(file_path):
     file = open(file_path, "r")
-    seconds = 0
+    total_seconds = 0
     i = 2
-    error = ''
     if not "time log:" in file.readline().lower():
-        print("Time log should be present in first line")
+        print("Text file should contain text as time log")
         return
-    for line in file:
-        line = line.replace('-', " ")
-        words = line.split()
-        first = 0
+    for each_line in file:
+        each_line = each_line.replace('-', " ")
+        word_array = each_line.split()
+        first = True
         flag = 0
-        for word in words:
+        log=''
+        for words in word_array:
             try:
-                if first == 0:
-                    start_time = datetime.strptime(word, "%I:%M%p")
-                    first += 1
+                if first:
+                    start = datetime.strptime(words, "%I:%M%p")
+                    first = False
                     flag = flag + 1
                 else:
-                    end_time = datetime.strptime(word, "%I:%M%p")
+                    end = datetime.strptime(words, "%I:%M%p")
                     flag = flag + 1
-                    if start_time <= end_time:
-                        diff = end_time - start_time
-                        seconds = seconds + diff.seconds
+                    if start <= end:
+                        diff = end - start
+                        total_seconds = total_seconds + diff.seconds
                     else:
-                        diff = end_time + timedelta(days=1) - start_time
-                        seconds = seconds + diff.seconds
+                        diff = end + timedelta(days=1) - start
+                        total_seconds = total_seconds + diff.seconds
             except ValueError as e:
                 pass
-
         if flag != 2:
-            print('Format mismatch at line %d' % i)
-            error = error + ('Format mismatch at line %d \n' % i)
+            print('Invalid time format at %d' % i)
+            log=log+'Invalid time format at %d\n' % i
         i = i + 1
+    return total_seconds,log
 
-    return seconds, error
+
+def convert(secs):
+    mints, sec = divmod(secs, 60)
+    hour, mints = divmod(mints, 60)
+    return "%d hours %d minutes " % (hour, mints)
 
 
 if __name__ == '__main__':
